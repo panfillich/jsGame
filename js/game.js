9d85659356ef0e7a44ef9c5de6a889cfa0190f8c
panfillich <tarakan29@gmail.com>
2016-04-18 17:25:33 +0300

    load + part map

--- js/game.js
+++ js/game.js
@@ -1,17 +1,22 @@
 //Загружаем игру
 
 var files = [
-    'constructors', //классы
+    'constuctors', //классы
     'static',       //глобальные переменные
     'type_hex',     //создаем типы гексагонов
     'map'           //создаем карту
 ];
 
+//Добавляем скрипты в нужном порядке
+files.forEach(function(file, number, files) {
+  addScript(file,'js');
+});
+
 //Добавить скрипт
-function addScript(name){
+function addScript(name,folder){
     var head= document.getElementsByTagName('head')[0];
     var script= document.createElement('script');
     script.type= 'text/javascript';
-    script.src= name+'.js';
+    script.src= folder+'/'+name+'.js';
     head.appendChild(script);
 }
