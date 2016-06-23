c6266f5c8851ca74be28eac61726e3d68ca69d95
panfillich <tarakan29@gmail.com>
2016-06-23 13:52:06 +0300

    del dev folder

--- game.js
+++
@@ -1,9 +0,0 @@
-function Game(id_game){
-	//Глобальные настройки
-	this.setting = new Setting(id_game);
-	this.animation = new Animation(this.setting);
-}
-
-
-var game_1 = new Game('game1');
-//var game_2 = new Game('game2');
